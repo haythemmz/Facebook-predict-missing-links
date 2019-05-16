8208717fb02fe8ad0d9f957ecd4277d88b50aaf1
haythemmz <haythemwc@gmail.com>
2019-05-16 10:34:05 +0100

    create subhash

--- data_exploration.py
+++ data_exploration.py
@@ -6,10 +6,21 @@ import networkx as nx
 hash_graphe=pickle.load( open( "hash_graph.pkl", "rb" ) )
 
 #%%
+l=list(hash_graphe.keys())[:1000]
+sub_hash={}
+for j in l:
+    sub_hash[j]=hash_graphe[j]
+#%%
+print(sub_hash)
+
+
+
+
+#%%
 g = nx.DiGraph()
 #%%
 g.add_nodes_from(hash_graphe.keys())
 #%%
 for k, v in hash_graphe.items():
     g.add_edges_from(([(k, t) for t in v]))
-pickle.dump( favorite_color, open( "graph.pkl", "wb" ) )
+pickle.dump( g, open( "graph.pkl", "wb" ) )
