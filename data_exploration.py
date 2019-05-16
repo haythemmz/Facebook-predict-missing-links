bf357cfd1659c1d1fd85d488b889840a10e62257
haythemmz <haythemwc@gmail.com>
2019-05-16 12:21:22 +0100

    " make graphe bigger"

--- data_exploration.py
+++ data_exploration.py
@@ -2,25 +2,61 @@
 import pandas as pd 
 import pickle 
 import networkx as nx
+import matplotlib.pyplot as plt 
 #%%
 hash_graphe=pickle.load( open( "hash_graph.pkl", "rb" ) )
 
+# dont forget to remove duplicated lines 
+
 #%%
-l=list(hash_graphe.keys())[:1000]
+l=list(hash_graphe.keys())[:100000]
 sub_hash={}
 for j in l:
     sub_hash[j]=hash_graphe[j]
 #%%
-print(sub_hash)
+print(len(list(sub_hash.keys())))
+print(len(list(hash_graphe.keys())))
 
 
 
 
 #%%
-g = nx.DiGraph()
+g_small = nx.DiGraph()
 #%%
-g.add_nodes_from(hash_graphe.keys())
+g_small.add_nodes_from(sub_hash.keys())
 #%%
-for k, v in hash_graphe.items():
-    g.add_edges_from(([(k, t) for t in v]))
-pickle.dump( g, open( "graph.pkl", "wb" ) )
+for k, v in sub_hash.items():
+    g_small.add_edges_from(([(k, t) for t in v]))
+pickle.dump( g_small, open( "graph_small.pkl", "wb" ) )
+
+#%%
+nx.info(g_small)
+
+#%%
+sub_grah=g_small.edge_subgraph(list(g_small.edges)[0:10])
+pos = nx.circular_layout(sub_grah)
+nx.draw(sub_grah,pos=pos,edges=sub_grah.edges, node_color = 'r',edge_color='b', wight=10)
+
+#%%
+print("number of persones {}".format(len(g_small.nodes)))
+# number of followers for each person 
+###################################################################################
+##                                                                               ##
+##                                 EDA                                           ##
+##                                                                               ##
+###################################################################################
+#%%
+
+followers_number=list(dict(g_small.in_degree()).values())
+followers_number.sort()
+#%%
+plt.figure(figsize=(10,6))
+plt.tight_layout()
+
+plt.plot(followers_number)
+plt.xlabel("network users")
+plt.ylabel("number of followers")
+plt.show()
+
+#%%
+
